bb21e2d0d0367d93e091585123525ed078f35716
Jack <ahmadzaky1108@gmail.com>
2024-10-06 22:28:54 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.title = "Ucok Mod SAMP";
+console.log(document.title)
+
+var loading = document.getElementById('loading');
+window.addEventListener("load",function() {
+  loading.style.display="none";
+});
+
+
+
